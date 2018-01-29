613e078ebd6edcf701c61f60e0477804c803479e
fengying <2@163.com>
2018-01-29 10:40:28 +0800

    update

--- src/containers/app/VisibleTodoList.js
+++ src/containers/app/VisibleTodoList.js
@@ -4,7 +4,7 @@
 import {connect} from "react-redux";
 import {toggleTodo} from "../../actions/app/index";
 import {TodoList} from "../../components/app/TodoList";
-
+import {bindActionCreators} from "redux"
 
 const getVisbleTodos =(todos,filter)=>{
     switch(filter){
@@ -29,8 +29,11 @@ const mapStateToProps =(state)=>{
 
 //用来建立 UI 组件的参数到store.dispatch方法的映射。也就是说，它定义了哪些用户的操作应该当作 Action，传给 Store。
 // 它可以是一个函数，也可以是一个对象。如果mapDispatchToProps是一个函数，会得到dispatch和ownProps（容器组件的props对象）两个参数
-const mapDispatchToProps ={
-        onTodoClick : toggleTodo
+const mapDispatchToProps = (dispatch)=>{
+    return {
+        onTodoClick : bindActionCreators(toggleTodo,dispatch)
+    }
+
 }
 
 //connect方法生成容器组件
